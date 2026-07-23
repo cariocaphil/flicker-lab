c9a56b3f493fdb61018c939a5df226195e43b86a
cariocaphil <cariocaphil@gmail.com>
2026-07-23 15:47:20 +0200

    add GitHub ci workflow

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-    css: true,
-  },
+    plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/test/setup.ts',
+        css: true,
+    },
 });
